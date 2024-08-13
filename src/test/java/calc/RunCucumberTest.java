2ced8a694e434f5adba93de76dd39c65aea71384
Naveen <karanamunaveen2002@gmail.com>
2024-08-13 10:17:22 +0530

    Added: Changes

--- src/test/java/calc/RunCucumberTest.java
+++ src/test/java/calc/RunCucumberTest.java
@@ -15,10 +15,11 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("calc")
 @SelectClasspathResource("other")
 //HTML file to generate the cucumber report
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/CucumberHtmlReports.html")
-//@ConfigurationParameter(key = "cucumber.filter.tags", value="")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/CucumberHtmlReports.html, json:target/CucumberJsonReports.json")
+//specific tags to include in the tests
 //@ConfigurationParameter(key = "cucumber.filter.tags", value="@calculator")
 //@ConfigurationParameter(key = "cucumber.filter.tags", value="@outline or @add and not @sub")
-@ConfigurationParameter(key = "cucumber.filter.tags", value="@outline or @add and not @sub or @imp")
+//@ConfigurationParameter(key = "cucumber.filter.tags", value="@outline or @add and not @sub or @imp")
+@ConfigurationParameter(key = "cucumber.filter.tags", value="")
 public class RunCucumberTest {
 }
